fcf2966a7f9aab507fdac855e196c54e2f80025c
ProfEscolaTec <alissoncosta@professor.educacao.sp.gov.br>
2024-06-06 19:15:39 -0300

    Testando o push... Ao enviar o push nas 2 vezes anteriores, abriu a janela de autorização e tive que mexer no Gerenciador de Credenciais do Windows

--- conexao_ok_https.py
+++ conexao_ok_https.py
@@ -1,3 +1,5 @@
 print("Deu tudo certo no push -u origin main com a chave HTTPS")
 
 print("Deu tudo certo no push -u origin main com a chave HTTPS alterando e salvando este arquivo")
+
+print("Testando o push... Ao enviar o push nas 2 vezes anteriores, abriu a janela de autorização e tive que mexer no Gerenciador de Credenciais do Windows")
